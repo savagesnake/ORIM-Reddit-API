1aad9191422f25d48f3389aad06e602fa7b49454
Wolfgang Criollo <wolfplisken@gmail.com>
2017-02-14 22:19:03 -0500

    set the fetch data use axios to generate the api, set state with all of the posts, pgLocation, before,after

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,7 +4,10 @@ class Posts extends Component {
   constructor(){
     super();
     this.state={
-      posts:[]
+      posts:[],
+      before:null,
+      after:null,
+      pgLocation:0
     }
 }
 
@@ -14,6 +17,9 @@ getPosts(url){
   .then((response) => {
     this.setState({
       posts:response.data.data.children,
+      before:response.data.data.before,
+      after:response.data.data.after
+
       //end of set state
     })
     //end of then
@@ -23,12 +29,49 @@ getPosts(url){
   });
   //end of getMovies
 }
+  componentDidDount(){
+    let apiURL = "https://www.reddit.com/r/all/hot.json?limit=5";
+    //call function and pass the url
+    this.getPosts(apiURL);
+  }
+
+  /**
+  * Listings do not use page numbers because their content changes so frequently.
+  * Instead, they allow you to view slices of the underlying data
+  * Listing JSON responses contain after and before fields
+  * which are equivalent to the "next" and "prev" buttons
+  **/
+  // get the next five post
+  nextPosts(){
+    //  set after value to fetch data
+      let after = this.state.after
+      const nextPost = `https://www.reddit.com/r/all/hot.json?count=200&after=${after+"&limit=5"}`;
+
+      // fetch data
+      this.getPosts(nextPost);
+
+      // add to pglocation to show the prev button
+      this.state.pgLocation++
+    }
+
+  // get the previous five post
+  prevPosts(){
+  //  set before value to fetch data
+      let before = this.state.before
+      const prevPost = `https://www.reddit.com/r/all/hot.json?count=200&before=${before+"&limit=5"}`;
+
+      // fetch data
+      this.getPosts(prevPost);
+
+      // substract from pglocation to hide the prev button
+      this.state.pgLocation--
+    }
 
   render(){
+    console.log(this.props)
     return(
       <div>Test</div>
     );
   }
 }
-
 export default Posts;
