74c6a43458588efbcaa829f1a28a50881be79c4a
Wolfgang Criollo <wolfplisken@gmail.com>
2017-02-14 20:37:02 -0500

    set up the axios api request promise set set state

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,29 @@
 import React, {Component} from "react";
-
+import Axios from "axios";
 class Posts extends Component {
+  constructor(){
+    super();
+    this.state={
+      posts:[]
+    }
+}
+
+// set function to get json data and updare state
+getPosts(url){
+  Axios.get(url)
+  .then((response) => {
+    this.setState({
+      posts:response.data.data.children,
+      //end of set state
+    })
+    //end of then
+  }).catch((errors)=>{
+    console.log(errors);
+    //end of catch
+  });
+  //end of getMovies
+}
+
   render(){
     return(
       <div>Test</div>
